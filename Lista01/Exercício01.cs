94fecfdc46634cf2dc43bdec9bc21e682bfc2e44
Gustavo Freitas <100533677+gustavfreitas@users.noreply.github.com>
2026-03-10 17:43:18 -0300

    Commit da Lista01 Completa de POO

---
+++ Lista01/Exercício01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+// Exercício 01
+
+/*
+{ 
+        Console.WriteLine("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Bem Vindo, {nome}!");
+
+}
+*/
+
+
+
